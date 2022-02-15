9cfff2098d7845ae04344eecd439d749b026469f
gauravchaulagain <gauravchaulagain45@gmail.com>
2022-02-15 15:54:36 +0545

    More on chatBar

--- lib/Screens/IndividualPage.dart
+++ lib/Screens/IndividualPage.dart
@@ -45,25 +45,71 @@ class _IndividualPageState extends State<IndividualPage> {
             ],
           ),
         ),
-        title: Container(
-          margin: EdgeInsets.all(5),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                widget.chatmodel.name!,
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              Text(
-                widget.chatmodel.lastMessage!,
-                style: TextStyle(fontSize: 15, color: Colors.grey),
-              ),
-              Text("Last Seen today at 12:00 PM",
-                  style: TextStyle(fontSize: 15))
-            ],
+        title: InkWell(
+          onTap: () {},
+          child: Container(
+            margin: EdgeInsets.all(6),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.chatmodel.name!,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  widget.chatmodel.currentMessage!,
+                  style: TextStyle(fontSize: 15, color: Colors.grey),
+                ),
+                Text("Last Seen today at 12:00 PM",
+                    style: TextStyle(fontSize: 15))
+              ],
+            ),
           ),
         ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.videocam),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.call),
+            onPressed: () {},
+          ),
+          PopupMenuButton<String>(
+            onSelected: (value) {
+              print(value);
+            },
+            itemBuilder: (BuildContext context) {
+              return [
+                PopupMenuItem(
+                  child: Text("View Contact"),
+                  value: "View Contact",
+                ),
+                PopupMenuItem(
+                  child: Text("Media ,Link and Docs"),
+                  value: "Media ,Link and Docs",
+                ),
+                PopupMenuItem(
+                  child: Text("Whats app web"),
+                  value: "Whats app web",
+                ),
+                PopupMenuItem(
+                  child: Text("Search"),
+                  value: "Search",
+                ),
+                PopupMenuItem(
+                  child: Text("Mute Notifications "),
+                  value: "Mute Notifications",
+                ),
+                PopupMenuItem(
+                  child: Text("Wallpaper "),
+                  value: "Wallpaper",
+                ),
+              ];
+            },
+          ),
+        ],
       ),
     );
   }
